3d6b37d23108b84cc8360c5fc52f2a470c3ae036
Diogo Rodrigues <128409445+rdrigues@users.noreply.github.com>
2023-07-13 19:00:15 -0300

    Create index.php

---
+++ ex005/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expressões Aritméticas</title>
+</head>
+<body>
+    <h1>Expressões Aritméticas</h1>
+    <?php 
+    //$res = 5 + 2 / 2;
+    //echo "O resultado é $res";
+    //$r = (base_convert(150, 10, 16));
+    //echo "resposta é $r";
+    //$r = intdiv(51, 2);
+    //echo "a resposta é $r";
+    $r = sqrt(15);
+    echo "a raiz de 5 é $r";
+    ?>
+    
+</body>
+</html>
