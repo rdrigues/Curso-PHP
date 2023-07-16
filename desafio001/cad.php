ebcef27d13f8619019c0de7cd400cbce2ed605b2
Diogo Rodrigues <128409445+rdrigues@users.noreply.github.com>
2023-07-16 17:47:03 -0300

    desafio

--- desafio001/cad.php
+++ desafio001/cad.php
@@ -11,30 +11,31 @@
         <h1>Resultado Final</h1>
     </header>
     <section>
-        <?php
-        $numero = $_GET ["numero"];
-        $antecessor = $numero - 1;
-        $sucessor = $numero + 1;
-        $raiz = sqrt($numero);
-        $raiz2 = round($raiz);
-        $quadrado = $numero * $numero;
-        $binario = base_convert($numero, 10, 2);
-        $hexodecimal = base_convert($numero, 10, 16);
-        $v100 = $numero * 100;
-        $d100 = $numero / 100;
-        echo "<i>O seu antecessor é <strong>$antecessor</strong> </i>    <br>";
-        echo "O número escolhido foi <strong>$numero</strong>            <br>";
-        echo "<i>O teu sucessor é <strong>$sucessor</strong></i>         <br>";
-        echo "E a raiz quadrada é <strong>$raiz2</strong>                <br>";
-        echo "Ele ao quadrado é <strong>$quadrado</strong>              <br>";
-        echo "Ele em binario <strong>$binario</strong>              <br>";
-        echo "Ele em hexodecimal <strong>$hexodecimal</strong>              <br>";
-        echo "Ele dividido por 100 <strong>$d100</strong>              <br>";
-        echo "Ele multiplicado por 100 <strong>$v100</strong>              <br>";
-
-
-
-        ?>
+        <form action="">
+            <?php
+            $numero = $_GET ["numero"];
+            $antecessor = $numero - 1;
+            $sucessor = $numero + 1;
+            $raiz = sqrt($numero);
+            $raiz2 = round($raiz);
+            $quadrado = $numero * $numero;
+            $binario = base_convert($numero, 10, 2);
+            $hexodecimal = base_convert($numero, 10, 16);
+            $v100 = $numero * 100;
+            $d100 = $numero / 100;
+            echo "<i>O seu antecessor é <strong>$antecessor</strong> </i>    <br>";
+            echo "O número escolhido foi <strong>$numero</strong>            <br>";
+            echo "<i>O teu sucessor é <strong>$sucessor</strong></i>         <br>";
+            echo "E a raiz quadrada é <strong>$raiz2</strong>                <br>";
+            echo "Ele ao quadrado é <strong>$quadrado</strong>              <br>";
+            echo "Ele em binario <strong>$binario</strong>              <br>";
+            echo "Ele em hexodecimal <strong>$hexodecimal</strong>              <br>";
+            echo "Ele dividido por 100 <strong>$d100</strong>              <br>";
+            echo "Ele multiplicado por 100 <strong>$v100</strong>              <br>";
+            ?>
+            
+        </form>
+        <button onclick="javascrip:window.location.href= 'index.php'">Voltar</button>
     </section>
     
 </body>
---
+++ desafio005/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>,
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <h1>Analisador de número real</h1>
+        <form action="cad.php" method="$_GET">
+          <label for="Valor">Número real : </label>
+          <input type="text" name="Valor" id="">
+          <input type="submit" value="Analisar">
+        </form>
+    </section>
+    
+</body>
+</html>
