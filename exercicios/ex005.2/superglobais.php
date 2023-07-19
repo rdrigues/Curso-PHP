dc9a65bb8e5b2e6358c3f6e0b40d670297b5334b
Diogo Rodrigues <128409445+rdrigues@users.noreply.github.com>
2023-07-19 11:12:48 -0300

    .

---
+++ exercicios/ex005.2/superglobais.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    <pre>
+
+    <?php 
+    echo "<h1> Superglobal GET</h1>";
+    
+    ?>
+
+    </pre>
+    </main>
+</body>
+</html>
